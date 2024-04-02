76c3f6790ef768b8e23f978343d2eb964e7882f5
Hyunsoo-Ryan-Lee <hyunsooyein@gmail.com>
2024-04-03 00:39:51 +0900

    240403

--- day-test/db/postgresql_query.py
+++
@@ -1,4 +0,0 @@
-queries = {
-    # "테이블 이름" : "테이블에 대한 쿼리"
-    "fake" : "SELECT * FROM fake",
-}
--- day-test/pipeline/remove.py
+++
@@ -1,9 +0,0 @@
-import shutil,os
-
-def remover(temp_path):
-    print("REMOVER START")
-    ## temporary storage 삭제
-    shutil.rmtree(temp_path)
-
-    ## temporary storage 빈 디렉토리 생성
-    os.makedirs(temp_path)
